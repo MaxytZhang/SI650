40605a7f7b6e3c023a11fe7ffedf07e5a383abee
高存志 <junji@gaocunzhideMacBook-Pro.local>
2019-12-10 17:46:47 -0500

    add extract keywords

--- back end/app.py
+++ back end/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify
+from extract import extract_kmeans
+
 
 app = Flask(__name__)
 
@@ -11,4 +13,16 @@ def rn_test():
     res = request.json
     print(res)
     return jsonify('Hello')
+
+
+@app.route('/keyword/', methods=['GET', 'POST'])
+def extract_keywords():
+    res = request.json
+    print(res)
+    query = "Andrew Yang"
+    keywords = extract_kmeans('1210election.data',query,10,3,3,True)
+    # print(keywords)
+    return jsonify(keywords)
+
+    
 
