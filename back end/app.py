397f808e277c1d80e25044cec89764d60cdf591c
Yangtao(Max) Zhang <maxzhang@umich.edu>
2019-12-11 12:43:09 -0500

    update backend

--- back end/app.py
+++ back end/app.py
@@ -1,23 +1,29 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 from bm25 import bm25_rank
 from extract import extract_kmeans
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 @app.route('/rn/', methods=['GET', 'POST'])
+@cross_origin()
 def rn_test():
     res = request.json
     print(res)
-    return jsonify('Hello')
+    response = jsonify('Hello')
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 @app.route('/rank/', methods = ['GET', 'POST'])
 def rank():
     res = request.json
 
-    rank = bm25_rank(res)
+    rank = bm25_rank(res["key"], 10)
     print(rank)
     return jsonify(rank)
 
